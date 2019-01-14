469a419c1d13350a800e5a1076d7edc662dcdb86
Yasthil Bhagwandeen <deiago147@gmail.com>
2019-01-14 00:03:29 -0800

    WIP - Added basic code for connecting.

---
+++ MultiplayerExperiments/Assets/PhotonManager.cs
@@ -0,0 +1,31 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotonManager : MonoBehaviour
+{
+
+    public GameObject _statusGO;
+    public string _gameVersion = "0.01";
+
+    public void ConnectToPhoton()
+    {
+        // connect to Photon
+        PhotonNetwork.GameVersion = _gameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+        UpdateStatus("Connecting...");
+    }
+
+    private void OnConnectedToMaster()
+    {
+        UpdateStatus("Connected to server!");
+    }
+
+    private void UpdateStatus(string status)
+    {
+        _statusGO.GetComponent<Text>().text = status;
+    }
+
+}
